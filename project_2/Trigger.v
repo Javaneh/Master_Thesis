e762da4866b44a070fc4b62205adca0084d87f33
Javaneh <javanehta@gmail.com>
2017-11-20 10:10:43 -0800

    Add files via upload

--- project_2/Trigger.v
+++ project_2/Trigger.v
@@ -20,36 +20,45 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Trigger( echo, clk, trigger, done );
-    parameter n = 9; // 9-bits
+module Trigger( echoDone, rst, clk, trigger, done );
+    parameter n1 = 9; // 9-bits
+    parameter n2 = 27; // 6-bits
     parameter [ 1:0 ] setTriggerLow = 2'd0, Idle = 2'd1, Delay = 2'd2;
-    input clk, echo;
+    input clk, echoDone, rst;
     output reg trigger, done;
-    reg [ n - 1:0 ] delayCnt = 9'd499;
+    reg [ n1 - 1:0 ] delayCnt = 9'b111110011; // 499
     reg [ 1:0 ] PS = setTriggerLow, NS;
-    reg [ 26:0 ] waitCnt = 27'd99999999; //27'd49999999 // 27'd100000000
+    reg [ n2 - 1:0 ] waitCnt = 27'b101111101011110000011111111; // 99999999
     
-    always @ ( PS ) begin
+    // for simulation purposes
+    //parameter n1 = 6; // 9-bits
+    //parameter n2 = 14; // 6-bits
+    //reg [ n1 - 1:0 ] delayCnt = 9'b110001;
+    //reg [ n2 - 1:0 ] waitCnt = 27'b10011100010000; // 9999
+    
+    always @ ( rst or echoDone ) begin
+        NS <= setTriggerLow;
+        PS <= NS;
+    end
+    
+    always @ ( posedge clk ) begin
         case ( PS )
             setTriggerLow: 
                 begin
-                    // set trigger low
-                    if ( echo )
-                    begin
-                        trigger <= 0;
-                        done <= 0;
-                        NS <= Idle;
-                    end
-                    else
-                        NS <= PS;
+                    NS <= Idle;
+                    trigger <= 0;
+                    done <= 0;
+                    delayCnt <= 9'b110001;
                 end
             Idle:
                 begin
                     // wait for 2 seconds
                     if ( waitCnt == 0 ) begin
                         NS <= Delay;
+                        waitCnt <= 27'b10011100010000;
                     end
                     else begin
+                        waitCnt <= waitCnt - 1;
                         NS <= PS;
                     end
                 end
@@ -57,32 +66,17 @@ module Trigger( echo, clk, trigger, done );
                 begin
                     // set trigger high & wait for 10 us
                     trigger <= 1;
+                    
                     if ( delayCnt == 0 ) begin
                         done <= 1;
                         NS <= setTriggerLow;
                     end
                     else begin
+                        delayCnt <= delayCnt - 1;
                         NS <= PS;
                     end
                 end
         endcase 
         PS <= NS;
-    end
-    always @ ( posedge clk ) begin
-        case ( PS )
-            setTriggerLow:
-                begin
-                    delayCnt <= 9'd499;
-                end
-            Idle:
-                begin
-                    waitCnt <= waitCnt - 1;
-                end
-            Delay:
-                begin
-                    waitCnt <= 27'd99999999;
-                    delayCnt <= delayCnt - 1;
-                end
-        endcase
     end
 endmodule
